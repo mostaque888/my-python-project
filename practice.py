0260b56f47d8c1254a424fe9393273a185910096
mostaque888 <mostaque888@gmail.com>
2025-04-29 02:15:16 +0100

    my python projectt

---
+++ practice.py
@@ -0,0 +1,18 @@
+animals.sort()  # Ensure the list is sorted for binary search
+
+animalToFind = input("What animal would you like to find? ").lower()
+
+validAnimal = False
+start = 0
+finish = len(animals) - 1
+
+while not validAnimal and start <= finish:
+    mid = (start + finish) // 2
+    if animals[mid] == animalToFind:
+        validAnimal = True
+    elif animalToFind > animals[mid]:
+        start = mid + 1
+    else:
+        finish = mid - 1
+
+print(validAnimal)
